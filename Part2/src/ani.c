e6c7e18b637552f49859477ddb0e3861fb340d8e
Aniruddhchavda <65471678+Aniruddhchavda@users.noreply.github.com>
2021-03-02 15:19:17 -0600

    Add files via upload

---
+++ Part2/src/ani.c
@@ -0,0 +1,2 @@
+Good Afternoon anchavda
+This is Tuesday 02 in March of 2021 (12:24:54 PM)
---
+++ Part2/src/ani2.c
@@ -0,0 +1,4 @@
+Hello, anchavda
+Current date is Tue Mar  2 12:24:47 CST 2021
+User is anchavda
+Current directory /home/TTU/anchavda/Project1/Part1
